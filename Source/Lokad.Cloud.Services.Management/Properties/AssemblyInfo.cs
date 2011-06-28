a904752b706c8e4293e79d7f7c3d9ef91dec06d3
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-06-28 15:02:02 +0200

    Services.Management: new project

---
+++ Source/Lokad.Cloud.Services.Management/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿#region Copyright (c) Lokad 2011
+// This code is released under the terms of the new BSD licence.
+// URL: http://www.lokad.com/
+#endregion
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lokad.Cloud.Services.Management")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCopyright("Copyright © Lokad 2011")]
+[assembly: AssemblyTrademark("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7dbce88-fae8-41ce-8394-4c645c3f1771")]
