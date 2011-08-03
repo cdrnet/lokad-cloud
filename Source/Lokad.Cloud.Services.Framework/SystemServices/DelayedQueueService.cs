5a56b2ad04e158ba33728068f3f78d5d45105513
Christoph Ruegg <christoph.ruegg@lokad.com>
2011-08-03 14:55:00 +0200

    Framework: ported delayed queue service to new framework

--- Source/Lokad.Cloud.Services.Framework/SystemServices/DelayedQueueService.cs
+++ Source/Lokad.Cloud.Services.Framework/SystemServices/DelayedQueueService.cs
@@ -4,41 +4,42 @@
 #endregion
 
 using System;
+using System.Threading;
 using Lokad.Cloud.ServiceFabric;
-using Lokad.Cloud.Services.Framework.Logging;
 using Lokad.Cloud.Services.Framework.Storage;
 using Lokad.Cloud.Storage;
 
 // HACK: the delayed queue service does not provide a scalable iteration pattern.
 // (single instance iterating over the delayed message)
 
-namespace Lokad.Cloud.Services
+namespace Lokad.Cloud.Services.Framework.SystemServices
 {
-    /// <summary>Routinely checks for expired delayed messages that needs to
-    /// be put in queue for immediate consumption.</summary>
-    [ScheduledServiceSettings(
-        AutoStart = true,
-        Description = "Checks for expired delayed messages to be put in regular queue.",
-        TriggerInterval = 15)] // 15s
-    public class DelayedQueueService : ScheduledService
+    /// <summary>
+    /// Routinely checks for expired delayed messages that needs to
+    /// be put in queue for immediate consumption.
+    /// </summary>
+    [ScheduledServiceSettings(TriggerInterval = 15)] // 15s
+    public class DelayedQueueService : ScheduledCloudService
     {
-        protected override void StartOnSchedule()
+        public override void OnSchedule(DateTimeOffset scheduledTime, CancellationToken cancellationToken)
         {
             // lazy enumeration over the delayed messages
             foreach (var parsedName in Blobs.ListBlobNames(new DelayedMessageName()))
             {
-                if (DateTimeOffset.UtcNow <= parsedName.TriggerTime)
+                // delayed messages are iterated in date-increasing order
+                // as soon a non-expired delayed message is encountered
+                // just stop the process.
+
+                if (DateTimeOffset.UtcNow <= parsedName.TriggerTime
+                    || cancellationToken.IsCancellationRequested)
                 {
-                    // delayed messages are iterated in date-increasing order
-                    // as soon a non-expired delayed message is encountered
-                    // just stop the process.
                     break;
                 }
 
                 var dm = Blobs.GetBlob(parsedName);
                 if (!dm.HasValue)
                 {
-                    Log.WarnFormat("Deserialization failed for delayed message {0}, message was dropped.", parsedName.Identifier);
+                    //Log.WarnFormat("Deserialization failed for delayed message {0}, message was dropped.", parsedName.Identifier);
                     continue;
                 }
 
